fd18730eba7eaa499f3115f1ca3d7fcb00c54e19
oemerfurkan <113520172+oemerfurkan@users.noreply.github.com>
2023-07-23 08:56:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 # Define the API endpoint to handle POST requests
 @app.route('/get_tendancy', methods=['POST'])
 def scrape_cooked_text_api():
@@ -20,7 +21,7 @@ def scrape_cooked_text_api():
     # Send a GET request to the URL and fetch the HTML content
     response = requests.get(url, headers=headers)
      # Raise an exception for any HTTP errors
-    print(response.raise_for_status())
+    response.raise_for_status()
 
     json_content = json.loads(response.content)
 
@@ -52,6 +53,48 @@ def scrape_cooked_text_api():
         return jsonify({'success': True, 'tendancy': positive / counter * 100})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+# Define the API endpoint to handle POST requests
+@app.route('/get_user_infos', methods=['POST'])
+def get_user_infos_api():
+    data = request.json  # Assuming JSON data with a "url" field containing the target URL
+    url = data.get('url', None)
+
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'}
+
+    # Send a GET request to the URL and fetch the HTML content
+    response = requests.get(url, headers=headers)
+     # Raise an exception for any HTTP errors
+    response.raise_for_status()
+
+    json_content = json.loads(response.content)
+
+    counter = 0
+    neg_scores = 0
+    pos_scores = 0
+
+    for item in json_content["post_stream"]["posts"]:
+        soup = BeautifulSoup(item["cooked"], "html.parser")
+        text = soup.find("p").text
+
+        classifier = pipeline(task="sentiment-analysis")
+        preds = classifier(text)
+        preds = [{"score": round(pred["score"], 4), "label": pred["label"]}
+            for pred in preds]
+        
+        if preds[0]["label"] == "POSITIVE":
+            pos_scores += item["score"]
+        elif preds[0]["label"] == "NEGATIVE":
+            neg_scores += item["score"]
+        counter += 1
+   
+    if url is None:
+        return jsonify({'error': 'Missing URL'}), 400
+
+    try:
+        return jsonify({'success': True, 'user_info': {"positive_scores": pos_scores, "negative_scores": neg_scores}})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0")
